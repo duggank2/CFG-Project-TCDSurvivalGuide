742fae23e204f0caad8987c987a5062602085572
Raluca Nicoara <nicoarar@tcd.ie>
2019-04-06 21:50:18 +0100

    template done

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,31 @@
-function myFunction() {
+function Faclick() {
+  document.getElementById("about")
+  window.open("https://www.facebook.com/trinitycollegedublin/","");
+}
+
+function Twclick() {
+  document.getElementById("about")
+  window.open("https://twitter.com/tcddublin","");
+}
+
+
+function Aboutclick() {
   document.getElementById("about")
   window.open("about.html","_self");
+}
+
+
+function Foodclick() {
+  document.getElementById("food")
+  window.open("food.html","_self");
+}
+
+function Disclick() {
+  document.getElementById("offers")
+  window.open("index.html","_self");
+}
+
+function Contactclick() {
+  document.getElementById("contact")
+  window.open("contact.html","_self");
 }
